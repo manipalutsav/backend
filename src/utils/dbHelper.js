e7a86e84858f0d08e617376f0520c6dcb43fe0e6
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-02-09 21:10:56 +0000

    lint: fix errors

--- src/utils/dbHelper.js
+++ src/utils/dbHelper.js
@@ -1,17 +1,23 @@
 let mongoose = require("mongoose");
-const server = "127.0.0.1:27017"; 
-const database = "utsav";
 
 class Database {
   constructor() {
+    this._ip = process.env.MONGODB_IP || "127.0.0.1";
+    this._port = process.env.MONGODB_PORT;
+    this._host = this._ip + ":" + this._port;
+    this._database = process.env.MONGODB_DATABASE;
+    this.uri = "mongodb://" + this._host + "/" + this._database;
+
     this._connect();
   }
+
   _connect() {
-    mongoose.connect(`mongodb://${server}/${database}`, { useNewUrlParser: true }).then(() => {
+    mongoose.connect(this.uri, { useNewUrlParser: true }).then(() => {
       console.log("Database connection successful");
     }).catch(err => {
       console.error("Database connection error: " + err);
     });
   }
 }
+
 module.exports = new Database();
