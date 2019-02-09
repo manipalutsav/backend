3fcc6572458490bd6e6045626b33bee250002841
Abid Hussain <abidhussain3008@gmail.com>
2019-02-10 02:41:41 +0530

    Add register route

--- src/utils/dbHelper.js
+++ src/utils/dbHelper.js
@@ -1,11 +1,23 @@
 let mongoose = require("mongoose");
-const server = "127.0.0.1:27017"; 
+const server = "127.0.0.1:27017";
 const database = "utsav";
 
+
+/**
+ * Class for mongo connectivity
+ *
+ */
 class Database {
+  /**
+   * Calls _connect method.
+  */
   constructor() {
     this._connect();
   }
+
+  /**
+   * connects to mongo server
+   */
   _connect() {
     mongoose.connect(`mongodb://${server}/${database}`, { useNewUrlParser: true }).then(() => {
       console.log("Database connection successful");
