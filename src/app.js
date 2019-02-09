092b412a9aeca62c2a9e5e0c297ed59092ccffba
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-02-09 18:11:41 +0000

    app: improvements
    * Configure dotenv
    * Configure morgan
    * Minor refactor

--- src/app.js
+++ src/app.js
@@ -1,17 +1,21 @@
 "use strict";
 
 const express = require("express");
-const app = express();
 const bodyParser = require("body-parser");
+const logger = require("morgan");
+const dotenv = require("dotenv");
+
+// Will be used soon.
+// eslint-disable-next-line no-unused-vars
 const db = require("./utils/dbHelper");
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// Create Express.js application
+const app = express();
 
-app.listen(8095, err => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Server spawned on port 8095");
-  }
-});
+// Configure application
+dotenv.config();
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+module.exports = app;
