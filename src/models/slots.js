2a8a9e3d1c5abbe8c575fd783989f5720ac6c112
Traction <19631364+k3rn31p4nic@users.noreply.github.com>
2019-02-12 01:45:05 +0000

    models: update slot model
    * Change team to String

--- src/models/slots.js
+++ src/models/slots.js
@@ -1,9 +1,9 @@
 let mongoose = require("mongoose");
 
-let slotsSchema = new mongoose.Schema({
+let slotSchema = new mongoose.Schema({
   slotNo: Number,
-  team: Number,
   round: String,
+  team: String,
 });
 
-module.exports = mongoose.model("Slots", slotsSchema);
+module.exports = mongoose.model("Slot", slotSchema);
