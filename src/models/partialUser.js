ccc7534ef64fe94d29ce7f699e152c7bd44912af
Traction <19631364+k3rn31p4nic@users.noreply.github.com>
2019-02-13 04:53:57 +0000

    models: add  partialUser model

---
+++ src/models/partialUser.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const schema = {
+  registrationID: {
+    // TODO: Add `match: RegExp` to validate registration number.
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    // TODO: Add `match: RegExp` to validate email.
+    type: String,
+    required: true,
+    lowercase: true,
+  },
+  mobile: {
+    // TODO: Add `match: RegExp` to validate mobile number.
+    type: String,
+    required: true,
+  },
+  college: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  faculty: {
+    type: Boolean,
+    required: true,
+  },
+};
+
+const options = {
+  autoCreate: true,
+};
+
+const partialUserSchema = new mongoose.Schema(schema, options);
+
+module.exports = mongoose.model("PartialUser", partialUserSchema);
