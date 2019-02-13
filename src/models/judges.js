fe0f629f0a9bdb2ea7e40cb0e9406648dd5d296c
Traction <19631364+k3rn31p4nic@users.noreply.github.com>
2019-02-13 04:55:27 +0000

    models: rewrite Judge model

--- src/models/judges.js
+++ src/models/judges.js
@@ -1,8 +1,18 @@
 const mongoose = require("mongoose");
 
-const judgeSchema = new mongoose.Schema({
-  name: String,
-  rounds: [ String ],
-});
+const schema = {
+  name: {
+    type: String,
+    required: true,
+  },
+  // TODO: Should it be rounds instead?
+  events: [ mongoose.Schema.Types.ObjectId ],
+};
+
+const options = {
+  autoCreate: true,
+};
+
+const judgeSchema = new mongoose.Schema(schema, options);
 
 module.exports = mongoose.model("Judge", judgeSchema);
