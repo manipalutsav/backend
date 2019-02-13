53b143a5343792343b17ff30ea9a02d423f267dd
Traction <19631364+k3rn31p4nic@users.noreply.github.com>
2019-02-13 04:54:19 +0000

    models: rewrite Team model

--- src/models/teams.js
+++ src/models/teams.js
@@ -1,10 +1,26 @@
 const mongoose = require("mongoose");
 
-const teamSchema = new mongoose.Schema({
-  collegeId: String,
-  participants: [ String ],
-  eventID: String,
-  disqualified: Boolean,
-});
+const schema = {
+  event: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  college: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  members: [ mongoose.Schema.Types.ObjectId ],
+  disqualified: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+};
+
+const options = {
+  autoCreate: true,
+};
+
+const teamSchema = new mongoose.Schema(schema, options);
 
 module.exports = mongoose.model("Team", teamSchema);
