6d65b645948e0927f5b52a9d476bf4b88e32b635
Traction <19631364+k3rn31p4nic@users.noreply.github.com>
2019-02-13 04:55:01 +0000

    models: add Round model

---
+++ src/models/round.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const schema = {
+  event: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  teams: [ mongoose.Schema.Types.ObjectId ],
+  duration: Number,
+  startDate: Date,
+  endDate: Date,
+  slottable: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+};
+
+const options = {
+  autoCreate: true,
+};
+
+const eventSchema = new mongoose.Schema(schema, options);
+
+module.exports = mongoose.model("Event", eventSchema);
