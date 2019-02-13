646d106903aa343922d199b4284e68ff34d70be0
Traction <19631364+k3rn31p4nic@users.noreply.github.com>
2019-02-13 04:52:48 +0000

    models: rewrite User model

--- src/models/users.js
+++ src/models/users.js
@@ -1,15 +1,39 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  // Registration Number
-  id: String,
-  name: String,
-  email: String,
-  contact: String,
-  type: Number,
-  password: String,
-  collegeId: String,
-  faculty: Boolean,
-});
+const schema = {
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    // TODO: Add `match: RegExp` to validate email.
+    type: String,
+    required: true,
+    lowercase: true,
+  },
+  mobile: {
+    // TODO: Add `match: RegExp` to validate mobile number.
+    type: String,
+    required: true,
+  },
+  type: {
+    type: Number,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  college: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+};
+
+const options = {
+  autoCreate: true,
+};
+
+const userSchema = new mongoose.Schema(schema, options);
 
 module.exports = mongoose.model("User", userSchema);
