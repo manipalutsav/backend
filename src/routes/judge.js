a64e49c878c4bfdc8273f68648bb6ebbd9921fc1
Adarsh <adarshbhalotia2910@gmail.com>
2019-02-19 20:32:26 +0530

    Add judge related routes

--- src/routes/judge.js
+++ src/routes/judge.js
@@ -1,1 +1,12 @@
 "use strict";
+
+const express = require("express");
+const router = express.Router();
+
+const {
+  register,
+} = require("../controllers/judges");
+
+router.post("/judge/register", register);
+
+module.exports = router;
--- src/routes/judges.js
+++ src/routes/judges.js
@@ -1,1 +1,12 @@
 "use strict";
+
+const express = require("express");
+const router = express.Router();
+
+const {
+  get,
+} = require("../controllers/judges");
+
+router.get("/judges", get);
+
+module.exports = router;
