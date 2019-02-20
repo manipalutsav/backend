90c3bc242576b94a69cc5459b47573570e25774e
Traction <19631364+k3rn31p4nic@users.noreply.github.com>
2019-02-20 04:18:24 +0000

    routes: add slot router

--- src/routes/slot.js
+++ src/routes/slot.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const express = require("express");
+const router = express.Router();
+
+const { getForTeam } = require("../controllers/slots");
+
+router.get("/round/:round/team/:team", getForTeam);
+
+module.exports = router;
