1c4d848f696e2fa2d8914d7585535fb57125b360
Traction <19631364+k3rn31p4nic@users.noreply.github.com>
2019-02-20 04:11:39 +0000

    routes: add router for leaderboard

--- src/routes/leaderboard.js
+++ src/routes/leaderboard.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const express = require("express");
+const router = express.Router();
+
+const { getAll } = require("../controllers/leaderboard");
+
+router.post("/", getAll)
+
+module.exports = router;
