7d895b98c297c57eeff927547f956c272675c99b
Traction <19631364+k3rn31p4nic@users.noreply.github.com>
2019-02-20 04:14:01 +0000

    controllers: changes to getForTeam method in slots

--- src/controllers/slots.js
+++ src/controllers/slots.js
@@ -27,7 +27,7 @@ const getByTeam = async (req, res) => {
 };
 
 const getForTeam = async (req, res) => {
-  let slots = await SlotModel.find({
+  let slots = await SlotModel.findOne({
     round: req.params.round,
     team: req.params.team,
   });
