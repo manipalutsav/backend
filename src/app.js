7b9dd44ff3ba8da069b748b3b8daf16e68b00982
Traction <19631364+k3rn31p4nic@users.noreply.github.com>
2019-02-20 04:19:19 +0000

    app: implement /slot & /slots router

--- src/app.js
+++ src/app.js
@@ -28,9 +28,13 @@ app.use(headers);
 // Routes
 const userRouter = require("./routes/user");
 const leaderboardRouter = require("./routes/leaderboard");
+const slotRouter = require("./routes/slot");
+const slotsRouter = require("./routes/slots");
 
 app.use("/user", userRouter);
 app.use("/leaderboard", leaderboardRouter);
+app.use("/slot", slotRouter);
+app.use("/slots", slotsRouter);
 
 // Error handlers
 app.use(handle404);
