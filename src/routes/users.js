1cc89fc8b6b619288b005d7fbc0e3ae36a215a18
Traction <19631364+k3rn31p4nic@users.noreply.github.com>
2019-02-22 15:48:29 +0000

    routes: add skeleton users router

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,1 +1,22 @@
 "use strict";
+
+const express = require("express");
+const router = express.Router();
+
+// TODO: replace `noop` with actual controllers
+const noop = require("../middlewares/noop");
+
+// Returns the user for the given id
+router.get("/:user", noop);
+
+// Create a new user
+router.post("/", noop);
+// Register a user into the system
+router.post("/register", noop);
+// Authenticate a user into the system
+router.post("/login", noop);
+
+// Modify the requester's user details.
+router.patch("/:user", noop);
+
+module.exports = router;
