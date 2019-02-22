39f1de45773b7c885e08a4cdbf3f2ddf7b45c0a9
Traction <19631364+k3rn31p4nic@users.noreply.github.com>
2019-02-22 15:42:38 +0000

    routes: add skeleton colleges router

--- src/routes/colleges.js
+++ src/routes/colleges.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const express = require("express");
+const router = express.Router();
+
+// TODO: replace `noop` with actual controllers
+const noop = require("../middlewares/noop");
+
+// Returns all colleges
+router.get("/", noop);
+// Returns the college for the given id
+router.get("/:college", noop);
+// Returns the list of participants from the given college
+router.get("/:college/participants", noop);
+// Returns the list of teams from the given college
+router.get("/:college/teams", noop);
+
+// Create a new college
+router.post("/", noop);
+
+module.exports = router;
