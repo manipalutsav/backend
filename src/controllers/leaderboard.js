775c58d070c2c108ef4a9bd2a5647e5321f66b96
Traction <19631364+k3rn31p4nic@users.noreply.github.com>
2019-02-23 14:03:11 +0000

    controllers: add init method to leaderboard controller

--- src/controllers/leaderboard.js
+++ src/controllers/leaderboard.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const LeaderboardModel = require("../models/Leaderboard");
+const CollegeModel = require("../models/College");
 
 /**
  * Returns the leaderboard
@@ -19,6 +20,31 @@ const get = async (req, res) => {
   });
 };
 
+/**
+ * Create the leaderboard with initial scores
+ * @param {object} req the request object
+ * @param {object} res the response object
+ * @returns {object} the response object
+ */
+const init = async (req, res) => {
+  let colleges = await CollegeModel.find();
+  colleges = colleges.map(clg => clg.id);
+
+  let leaderboard = colleges.map(clg => ({
+    college: clg,
+    points: 0,
+  }));
+
+  await LeaderboardModel.create(leaderboard);
+
+  return res.json({
+    status: 200,
+    message: "Success",
+    data: leaderboard,
+  });
+};
+
 module.exports = {
   get,
+  init,
 };
