39b2660f2f904f237074b41bbabddc70877267f5
Traction <19631364+k3rn31p4nic@users.noreply.github.com>
2019-02-23 00:47:15 +0000

    routes: implement GET /leaderboard route

--- src/routes/leaderboard.js
+++ src/routes/leaderboard.js
@@ -5,9 +5,10 @@ const router = express.Router();
 
 // TODO: replace `noop` with actual controllers
 const noop = require("../middlewares/noop");
+const Leaderboard = require("../controllers/leaderboard");
 
 // Returns the leaderboard
-router.get("/", noop);
+router.get("/", Leaderboard.get);
 
 // Create the leaderboard with initial scores
 router.post("/", noop);
