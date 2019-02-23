37260fea687e4fe73d3ed159f6c071b38fd48079
Traction <19631364+k3rn31p4nic@users.noreply.github.com>
2019-02-23 13:47:11 +0000

    controllers: add update method to users controller

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -94,6 +94,74 @@ const create = async (req, res, next) => {
   }
 };
 
+const update = async (req, res, next) => {
+  if (!req.body.oldUser || !req.body.newUser) {
+    return res.status(400).json({
+      status: 400,
+      message : "Bad request",
+    });
+  }
+
+  if (req.params.user !== req.body.oldUser.id) {
+    return res.status(401).json({
+      status: 401,
+      message : "Unauthorized",
+    });
+  }
+
+  let user = await UserModel.findById(req.params.user);
+
+  if (!user) {
+    return res.status(401).json({
+      status: 401,
+      message : "Unauthorized",
+    });
+  }
+
+  let isValidPassword = await hash.comparePasswordHash(
+    req.body.oldPassword,
+    user.password
+  );
+
+  if (!isValidPassword) {
+    return res.status(401).json({
+      status: 401,
+      message : "Unauthorized",
+    });
+  }
+
+  if (user.name !== req.body.newUser.name) user.name = req.body.newUser.name;
+  if (user.email !== req.body.newUser.email) user.email = req.body.newUser.email;
+  if (user.mobile !== req.body.newUser.mobile) user.mobile = req.body.newUser.mobile;
+  if (user.college !== req.body.newUser.college) user.college = req.body.newUser.college;
+
+  await user.save().
+    then(user => {
+      const token = jwt.generateToken(user.email);
+
+      return res.cookie("token", token).json({
+        status: 200,
+        message: "User details updated",
+        data: {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          mobile: user.mobile,
+          type: user.type,
+          college: user.college,
+        },
+      });
+    }).
+    catch((e) => {
+      console.error(e);
+
+      return res.status(500).json({
+        status: 500,
+        message: "Internal Server Error",
+      });
+    });
+};
+
 /**
  * Authenticate a user into the system
  * @param {object} req The request object
@@ -140,5 +208,6 @@ const login = async (req, res, next) => {
 module.exports = {
   get,
   create,
+  update,
   login,
 };
