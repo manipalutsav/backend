9df57fe80c0bd9027b05fc7999cf797547512b2c
Traction <19631364+k3rn31p4nic@users.noreply.github.com>
2019-02-23 15:50:51 +0000

    routes: implement PATCH /leaderboard/:college route

--- src/routes/leaderboard.js
+++ src/routes/leaderboard.js
@@ -13,7 +13,7 @@ router.get("/", Leaderboard.get);
 // Create the leaderboard with initial scores
 router.post("/", Leaderboard.init);
 
-// Modify the leaderboard with updated scores
-router.patch("/", noop);
+// Modify the leaderboard with updated scores of the given college
+router.patch("/:college", Leaderboard.update);
 
 module.exports = router;
