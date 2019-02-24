0d960f443d84f8f2a5d69f91fc7c0a0ac76eddf4
Traction <19631364+k3rn31p4nic@users.noreply.github.com>
2019-02-24 03:22:42 +0000

    routes: implement a few /colleges route

--- src/routes/colleges.js
+++ src/routes/colleges.js
@@ -5,17 +5,18 @@ const router = express.Router();
 
 // TODO: replace `noop` with actual controllers
 const noop = require("../middlewares/noop");
+const Colleges = require("../controllers/colleges");
 
 // Returns all colleges
 router.get("/", noop);
 // Returns the college for the given id
-router.get("/:college", noop);
+router.get("/:college", Colleges.get);
 // Returns the list of participants from the given college
 router.get("/:college/participants", noop);
 // Returns the list of teams from the given college
 router.get("/:college/teams", noop);
 
 // Create a new college
-router.post("/", noop);
+router.post("/", Colleges.create);
 
 module.exports = router;
