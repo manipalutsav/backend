8bcf08560adaf8a4dc84658528e02aa91e9d7a80
Meghashyam Bhandary <r21meghashyam@gmail.com>
2019-03-13 22:13:17 +0530

    added github link

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ app.use(cors({
   origin: [
     "http://manipalutsav.com",
     "https://manipalutsav.com",
+    "https://manipalutsav.github.io",
     /\.manipalutsav\.com$/,
     /^(https?:\/\/)(localhost|(192\.168\.\d{1,3}\.\d{1,3}))(:\d{1,5})*$/,
     // /^(?:https?:\/\/)?(?:localhost|(?:192\.168(?:\.(?:\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])){2})(?::(?:[0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5]))$/,
