6762b7bfddf1429896f90890921e40a353b0cd03
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-03-18 21:08:15 +0530

    routes(colleges): add GET /participants

--- src/routes/colleges.js
+++ src/routes/colleges.js
@@ -7,6 +7,8 @@ const Colleges = require("../controllers/colleges");
 
 // Returns all colleges
 router.get("/", Colleges.getAll);
+// Returns the list of participants from all colleges
+router.get("/participants", Colleges.getParticipants);
 // Returns the college for the given id
 router.get("/:college", Colleges.get);
 // Returns the list of participants from the given college
