b5be9a50b22c48ecf2ec11382e4e257fae6f37c5
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-04-07 17:28:45 +0530

    controllers(participants): add get method

--- src/controllers/participants.js
+++ src/controllers/participants.js
@@ -2,6 +2,30 @@
 
 const ParticipantModel = require("../models/Participant");
 
+const get = async (req, res) => {
+  try {
+    let participant = await ParticipantModel.findById(req.params.participant);
+
+    if (!participant) {
+      return res.status(404).json({
+        status: 404,
+        message: "Not Found. No participant was found for the given ID.",
+      });
+    }
+
+    return res.json({
+      status: 200,
+      message: "Success",
+      data: participant,
+    });
+  } catch (e) {
+    return res.status(500).json({
+      status: 500,
+      message: "Internal Server Error",
+    });
+  }
+};
+
 const getAll = async (req, res) => {
   try {
     let participants = await ParticipantModel.find();
@@ -49,6 +73,7 @@ const update = async (req, res) => {
 };
 
 module.exports = {
+  get,
   getAll,
   update,
 };
