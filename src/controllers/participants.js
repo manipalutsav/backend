ecb59d41a5d251d2679e5d72049c32969d0ac1bf
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-04-07 17:21:51 +0530

    controllers(participants): add update method

--- src/controllers/participants.js
+++ src/controllers/participants.js
@@ -19,6 +19,36 @@ const getAll = async (req, res) => {
   }
 };
 
+const update = async (req, res) => {
+  try {
+    let participant = await ParticipantModel.findById(req.params.participant);
+
+    if (!participant) {
+      return res.status(404).json({
+        status: 404,
+        message: "Not Found. No participant was found for the given ID.",
+      });
+    }
+
+    if (req.body.name) participant.name = req.body.name;
+    if (req.body.registrationID) participant.registrationID = req.body.registrationID;
+
+    await participant.save();
+
+    return res.json({
+      status: 200,
+      message: "Success",
+      data: participant,
+    });
+  } catch (e) {
+    return res.status(500).json({
+      status: 500,
+      message: "Internal Server Error",
+    });
+  }
+};
+
 module.exports = {
   getAll,
+  update,
 };
