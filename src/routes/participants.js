1941d69c8f41765de92f02f6d690331ffcd22894
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-04-07 17:29:19 +0530

    routes(participants): add GET /participants/:participant route

--- src/routes/participants.js
+++ src/routes/participants.js
@@ -7,6 +7,8 @@ const Participants = require("../controllers/participants");
 
 // Returns the list of all participants
 router.get("/", Participants.getAll);
+// Returns the specified participant
+router.get("/:participant", Participants.get);
 
 // Updates the details of the specified participant
 router.patch("/:participant", Participants.update);
