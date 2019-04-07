41a9d02ae0d368b346e6f22778c2474f2f4c91a3
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-04-07 17:22:34 +0530

    routes(participants): add PATCH /participants/:participant route

--- src/routes/participants.js
+++ src/routes/participants.js
@@ -8,4 +8,7 @@ const Participants = require("../controllers/participants");
 // Returns the list of all participants
 router.get("/", Participants.getAll);
 
+// Updates the details of the specified participant
+router.patch("/:participant", Participants.update);
+
 module.exports = router;
