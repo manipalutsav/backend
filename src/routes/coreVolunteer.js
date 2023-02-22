5fbdcc27cb2048f5be7ddd9a083ebd4b87b282a6
Meghashyam Bhandary <r21meghashyam@gmail.com>
2023-02-23 05:16:06 +0530

    Added route

--- src/routes/coreVolunteer.js
+++ src/routes/coreVolunteer.js
@@ -3,11 +3,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { addVolunteer, updateVolunteer, deleteVolunteer, getVolunteers } = require("../controllers/coreVolunteer");
+const { addVolunteer, updateVolunteer, deleteVolunteer, getVolunteers, getVolunteer } = require("../controllers/coreVolunteer");
 
 router.post("/", addVolunteer);
 router.patch("/:volunteerId", updateVolunteer);
 router.delete("/:volunteerId", deleteVolunteer);
+router.get("/:volunteerId", getVolunteer);
 router.get("/", getVolunteers);
 
 module.exports = router;
