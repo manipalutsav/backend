e35cd06a1c89a85e1a59180e7308ac681bae3cf8
Meghashyam Bhandary <r21meghashyam@gmail.com>
2023-02-23 05:32:07 +0530

    updated http codes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,26 +1,26 @@
 const HTTP_STATUS = {
   200: {
-    status: "200",
+    status: 200,
     message: "Success",
   },
   400: {
-    status: "400",
+    status: 400,
     message: "Bad Request",
   },
   401: {
-    status: "401",
+    status: 401,
     message: "Unauthorized, Try logging in again",
   },
   403: {
-    status: "403",
+    status: 403,
     message: "Forbidden",
   },
   404: {
-    status: "404",
+    status: 404,
     message: "Not Found",
   },
   500: {
-    status: "500",
+    status: 500,
     message: "Internal Server Error",
   },
 };
