ffcb632c66fe19c46a80d85843b163b401f28827
Meghashyam Bhandary <r21meghashyam@gmail.com>
2023-02-27 06:53:56 +0530

    test

--- scripts/restart.js
+++ scripts/restart.js
@@ -13,7 +13,7 @@ pm2.connect(async (err) => {
         }
         let list = await new Promise((res, rej) => pm2.list((err, list) => err ? rej(err) : res(list)))
 
-        let pm2Instance = list.find(item => item.name.match(/sudo node/));
+        let pm2Instance = list.find(item => item.name.match(/node/));
 
         if (!pm2Instance) {
             console.log("No instance found, creating one...")
