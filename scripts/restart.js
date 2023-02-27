145bf7e65231e63613a5aaf76c1ef89c04dfcbbf
Meghashyam Bhandary <r21meghashyam@gmail.com>
2023-02-27 06:58:18 +0530

    test

--- scripts/restart.js
+++ scripts/restart.js
@@ -23,7 +23,7 @@ pm2.connect(async (err) => {
             let pid = pm2Instance.pid;
             let pm2_id = pm2Instance.pm2_env.pm_id;
             console.log("Stopping it.")
-            await new Promise((res, rej) => pm2.stop(pm2_id, (err) => err ? rej(err) : res(0)))
+            await new Promise((res, rej) => pm2.stop(pm2_id, (err) => err != "Terminated" ? rej(err) : res(0)))
             child_process.execSync("sudo kill " + pid);
             console.log(child_process.execSync("pm2 restart " + pm2_id).toString());
             console.log("Restarted")
