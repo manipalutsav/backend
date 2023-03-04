a1fd82345f9b428ac05ae72b20a4ed9cc30b0adb
Meghashyam Bhandary <r21meghashyam@gmail.com>
2023-03-05 03:29:29 +0530

    Updated backup type

---
+++ scripts/restore.js
@@ -0,0 +1,30 @@
+const child_process = require("child_process");
+const fs = require("fs");
+const dotenv = require("dotenv");
+dotenv.config();
+
+
+const importCollection = (collectionName) => {
+    return new Promise((resolve, reject) => {
+        let db = process.env.MONGODB_DATABASE || "utsav";
+        if (JSON.parse(fs.readFileSync("../backup/dump/" + collectionName + ".json")).length == 0)
+            return resolve();
+        child_process.exec(`mongoimport --db=${db} --collection=${collectionName}  --jsonArray --file=${collectionName}.json`, {
+            cwd: "../backup/dump"
+        }, (err) => {
+            if (err) {
+                reject(err);
+            }
+            console.log(collectionName + " restored");
+            resolve();
+        });
+    })
+}
+
+const main = async () => {
+    let collections = fs.readdirSync("../backup/dump").filter(file => file.endsWith(".json")).map(file => file.slice(0, -5));
+    await Promise.all(collections.map(collectionName => importCollection(collectionName)));
+    console.log("Done");
+}
+
+main();
