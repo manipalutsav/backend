0a569dde56044a413c8745e80468951de8b693a2
Chandradip Roy <diproy2002@gmail.com>
2025-03-14 21:30:15 +0530

    Reintroduce the requireAdmin middleware for authentication

--- src/routes/judges.js
+++ src/routes/judges.js
@@ -4,6 +4,7 @@ const express = require("express");
 const router = express.Router();
 
 const Judges = require("../controllers/judges");
+const requireAdmin  = require("../middlewares/judges");
 
 // Returns the list of all judges
 router.get("/", Judges.getAll);
@@ -14,6 +15,6 @@ router.post("/", Judges.create);
 
 /*The requester must be an admin to delete a judge. 
 This is done by using the middleware requireAdmin*/
-router.delete("/:id",  Judges.deleteJudgeById);
+router.delete("/:id", requireAdmin, Judges.deleteJudgeById);
 
 module.exports = router;
